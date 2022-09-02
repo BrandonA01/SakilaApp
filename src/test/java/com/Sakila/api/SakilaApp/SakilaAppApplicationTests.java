f8eeb8b542881e37947304965c25abea7df321c1
Brandon Allen <81233247+BrandonA01@users.noreply.github.com>
2022-09-02 10:22:27 +0100

    Setting up sonar cloud

--- src/test/java/com/Sakila/api/SakilaApp/SakilaAppApplicationTests.java
+++ src/test/java/com/Sakila/api/SakilaApp/SakilaAppApplicationTests.java
@@ -3,11 +3,13 @@ package com.Sakila.api.SakilaApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SakilaAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals("hello", "hello", "hello!=hello");
 	}
-
 }
