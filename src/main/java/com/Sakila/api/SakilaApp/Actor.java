aba83235ef0378ea13f41f509fa8e842afd5484e
Brandon Allen <81233247+BrandonA01@users.noreply.github.com>
2022-09-02 09:59:20 +0100

    First Commit

---
+++ src/main/java/com/Sakila/api/SakilaApp/Actor.java
@@ -0,0 +1,54 @@
+package com.Sakila.api.SakilaApp;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "actor")
+public class Actor {
+    //Attributes
+    @Id
+    @Column(name = "actor_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int actorid;
+    @Column(name = "first_name")
+    String first_name;
+    @Column(name = "last_name")
+    String last_name;
+
+    //Constructors
+    public Actor(String first_name, String last_name) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+
+    }
+
+    public Actor() {
+    }
+
+    //Methods
+
+
+    public int getActorid() {
+        return actorid;
+    }
+
+    public void setActorid(int actorid) {
+        this.actorid = actorid;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+}
